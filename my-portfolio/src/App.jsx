74cb024c2389fdf10a4d49f5d0ac881686e0ffce
ArnoldOnchieku <kraftycoder8@gmail.com>
2025-06-21 10:27:45 +0300

    feat: add tailwind css files

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 class="text-3xl font-medium">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="text-3xl font-medium color-" >
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-600 to-pink-600">
+      <h1 className="text-4xl font-bold text-white drop-shadow-lg">
+        Tailwind v3.4.17 + Vite!
+      </h1>
+       <h1 className="text-3xl font-bold text-white drop-shadow-lg">
+        Tailwind v3.4.17 + Vite!
+      </h1>
+       <h1 className="text-2xl font-bold text-white drop-shadow-lg">
+        Tailwind v3.4.17 + Vite!
+      </h1>
+       <h1 className="text-xl font-bold text-white drop-shadow-lg">
+        Tailwind v3.4.17 + Vite!
+      </h1>
+    </div>
+  );
 }
-
-export default App
